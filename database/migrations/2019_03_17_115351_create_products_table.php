f46b868a5e952b41ff34f84dc22441d595905588
Clément Mortas <clement.mortas74150@gmail.com>
2019-03-17 13:38:11 +0100

    first-commit

---
+++ database/migrations/2019_03_17_115351_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->Increments('id')->unsigned();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->Integer('user_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('products', function (Blueprint $table){
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2019_03_17_115442_create_products_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_categories', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->Integer('product_id')->unsigned();
+            $table->Integer('category_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('products_categories', function (Blueprint $table) {
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_categories');
+    }
+}
